bb40471319434bb919207218080850d181504046
Shivam Maurya <66348742+maurya050@users.noreply.github.com>
2025-12-10 10:12:58 +0530

    Time: 4 ms (20.72%) | Memory: 65.3 MB (9.58%) - LeetSync

---
+++ 53-maximum-subarray/maximum-subarray.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let res = nums[0];
+    let total = 0;
+
+    for(let n of nums){
+        if(total < 0)
+            total = 0;
+        
+        total += n;
+        res = Math.max(total, res);
+    }
+    return res;
+};
