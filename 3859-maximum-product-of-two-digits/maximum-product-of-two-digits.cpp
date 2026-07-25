c1ec6c30dbceb49c52f296c36f6a48d687a0dc8e
Shivam Maurya <66348742+maurya050@users.noreply.github.com>
2026-07-25 20:49:51 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (89.66%) - LeetSync

--- 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
+++ 3859-maximum-product-of-two-digits/maximum-product-of-two-digits.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     int maxProduct(int n) {
-        vector<int> arr(10, 0);
-
+        int max1 =0, max2 = 0;
         while(n){
             int r = n%10;
-            arr[r] += 1;
-            n = n/10;
-            cout<< arr[r];
-        }
-        int ans = 1;
-        int ind = 9;
-        int count =0;
-        while(ind >=0 && count <2){
-            // count<< arr[ind];
-            if(arr[ind] != 0){
-                ans *= ind;
-                arr[ind] -= 1;
-                count++;
+            if(r >= max1){
+                max2 = max1;
+                max1 = r;
             }
-            if(arr[ind] == 0)
-                ind--;
+            else if(r > max2)
+                max2 = r;
+            n = n/10;
         }
-        return ans;
+        return max1 * max2;
+       
     }
 };
